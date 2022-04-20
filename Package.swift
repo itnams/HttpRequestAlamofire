275f6e80b052fb956f49f55f092615f78ece211d
Nam Nguyen <itnams12@gmail.com>
2022-04-20 11:12:51 +0700

    feat: format code

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(
             name: "HttpRequestAlamofire",
             targets: ["HttpRequestAlamofire"]),
-        ,
+    
     ],
     dependencies: [
         //         Dependencies declare other packages that this package depends on.
